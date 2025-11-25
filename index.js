4da6537302444946beebe579304529a5bf308ea1
Alokkumartiwari07 <kumartiwarialok251@gmail.com>
2025-11-26 00:16:17 +0530

    .gitignore file

--- index.js
+++ index.js
@@ -10,6 +10,12 @@ app.get("/", (req, res) => {
 app.get("/twitter", (req, res) => {
   res.send("welcome to twitter");
 });
+app.get("/introduction", function (req, res) {
+  res.send("alok kumar tiwari");
+});
+app.get("/rku", function () {
+  res.send("welcome to rk university");
+});
 app.get("/facebook", (req, res) => {
   res.send("<h1> welcome to facebook.plz login in Facebook</h1>");
 });
@@ -17,7 +23,9 @@ app.get("/facebook", (req, res) => {
 app.get("/youtube", (req, res) => {
   res.send("<h2>CHAI AUR CODE</h2>");
 });
-
+// app.listen(port,()=>{
+//   console.log(`Example app listerning on port ${port}`);
+// }
 app.listen(process.env.PORT, () => {
   console.log(`Example app listerning on port ${port}`);
 });
